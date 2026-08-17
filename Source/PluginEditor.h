13d9504d08a54738b135eba93329521f8947c3fb
stmoeller60 <stmoeller62@gmail.com>
2026-08-17 23:03:06 +0200

    Add Simple and Advanced editor controls

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -17,18 +17,30 @@ public:
 private:
     void timerCallback() override;
     void setupSlider (juce::Slider& slider, juce::Label& label, const juce::String& name);
+    void updateModeVisibility();
 
-    using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+    using SliderAttachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+    using ButtonAttachment = juce::AudioProcessorValueTreeState::ButtonAttachment;
 
     SteveSledgeCompressorAudioProcessor& processor;
 
+    juce::ToggleButton simpleModeButton { "Simple" };
+
+    // Simple mode controls.
+    juce::Slider compSlider, attackSlider, levelSlider;
+    juce::Label compLabel, attackLabel, levelLabel;
+
+    // Shared + Advanced mode controls.
     juce::Slider inputSlider, thresholdSlider, ratioSlider, speedSlider, makeupSlider, ceilingSlider;
     juce::Label inputLabel, thresholdLabel, ratioLabel, speedLabel, makeupLabel, ceilingLabel;
 
-    std::unique_ptr<Attachment> inputAttachment, thresholdAttachment, ratioAttachment,
-                                speedAttachment, makeupAttachment, ceilingAttachment;
+    std::unique_ptr<ButtonAttachment> simpleAttachment;
+    std::unique_ptr<SliderAttachment> compAttachment, attackAttachment, levelAttachment,
+                                      inputAttachment, thresholdAttachment, ratioAttachment,
+                                      speedAttachment, makeupAttachment, ceilingAttachment;
 
     std::array<float, 5> meterDb { 0.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+    bool lastSimpleMode = false;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SteveSledgeCompressorAudioProcessorEditor)
 };
